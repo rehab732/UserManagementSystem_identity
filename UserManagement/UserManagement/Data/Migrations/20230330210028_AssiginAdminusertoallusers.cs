36d0bddef7bd908634bab3614d52185f9b0193b9
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-30 23:15:38 +0200

    AssiginAdminusertoallusers

---
+++ UserManagement/UserManagement/Data/Migrations/20230330210028_AssiginAdminusertoallusers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.Data.Migrations
+{
+    public partial class AssiginAdminusertoallusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [security].[UserRoles](UserId,RoleId) SELECT 'e57036f67ac7',Id from [security].[Roles]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [security].[UserRoles] WHERE UserId = 'e57036f67ac7'");
+        }
+    }
+}
